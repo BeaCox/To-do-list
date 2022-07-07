85343469cb337d56b7758ff38bdbee43d2549006
cyChen <2273202874@qq.com>
2022-07-07 19:42:05 +0800

    2.0添加了文件输入输出，还有些警报

--- task.cpp
+++ task.cpp
@@ -1,170 +1,311 @@
 #include "task.h"
-    char defaultName[20]="学习C++";
-    char defaultClassify[20]="学习";
-    int LIST_SIZE=100;
-    timeT::timeT(int y,int M,int d,int h,int m){
-        year=y;
-        month=M;
-        day=d;
-        hour=h;
-        minute=m;
-    }
+#include <cstring>
+#include <stdio.h>
+#include "stringAndInt.h"
+char defaultName[20] = "学习C++";
+char defaultClassify[20] = "学习";
+int LIST_SIZE = 100;
+timeT::timeT(int y, int M, int d, int h, int m)
+{
+    year = y;
+    month = M;
+    day = d;
+    hour = h;
+    minute = m;
+}
 
+int task::taskNum = 0;
+int task::taskLive = 0;
 
-    int task::taskNum=0;
-    int task::taskLive=0;//
-
-    task::task()
-    :start(2022,1,1,0,0),timerReminder(2022,1,1,0,0)
-    {
-    this->id=0;
+task::task()
+    : start(2022, 1, 1, 0, 0), timerReminder(2022, 1, 1, 0, 0)
+{
+    this->id = 0;
     int i = 0;
-	for (; name[i] != '\0'; ++i) {
-		this->name[i] = name[i];
-	}
-	this->name[i] = '\0';
-    i = 0;
-	for (; classify[i] != '\0'; ++i) {
-		this->classify[i] = classify[i];
-	}
-    this->priority=3;
-    }
-
-    void task::display(){
-        cout << "任务id: " << id << endl;
-        cout << "任务名称: " << name << endl;
-        cout << "任务开始时间: " << start.year << "-" << start.month << "-" << start.day << " " << start.hour << ":" << start.minute << endl;
-        cout << "任务分类: " << classify << endl;
-        cout << "任务优先级: " << priority << endl;
-        cout << "任务提醒时间: " << timerReminder.year << "-" << timerReminder.month << "-" << timerReminder.day << " " << timerReminder.hour << ":" << timerReminder.minute << endl << endl;
-    }
-
-
-    void task::setTask(int id=0,char *name=defaultName,int sYear=2022,int sMonth=1,int sDay=1,int sHour=0,int sMinute=0,int rYear=2022,int rMonth=1,int rDay=1,int rHour=0,int rMinute=0,char *classify=defaultClassify,int priority=1)
+    for (; name[i] != '\0'; ++i)
     {
-    this->id=id;//！！！！！！！！！！随机分配id应该写在这里！！！！！！！！！！！
-    int i = 0;
-	for (; name[i] != '\0'; ++i) {
-		this->name[i] = name[i];
-	}
-	this->name[i] = '\0';
-    this->start=start;
+        this->name[i] = name[i];
+    }
+    this->name[i] = '\0';
     i = 0;
-	for (; classify[i] != '\0'; ++i) {
-		this->classify[i] = classify[i];
-	}
+    for (; classify[i] != '\0'; ++i)
+    {
+        this->classify[i] = classify[i];
+    }
+    this->priority = 3;
+}
+
+void task::display()
+{
+    cout << "任务id: " << id << endl;
+    cout << "任务名称: " << name << endl;
+    cout << "任务开始时间: " << start.year << "-" << start.month << "-" << start.day << " " << start.hour << ":" << start.minute << endl;
+    cout << "任务分类: " << classify << endl;
+    cout << "任务优先级: " << priority << endl;
+    cout << "任务提醒时间: " << timerReminder.year << "-" << timerReminder.month << "-" << timerReminder.day << " " << timerReminder.hour << ":" << timerReminder.minute << endl
+         << endl;
+}
+
+void task::setTask(int id = 0, char *name = defaultName, int sYear = 2022, int sMonth = 1, int sDay = 1, int sHour = 0, int sMinute = 0, int rYear = 2022, int rMonth = 1, int rDay = 1, int rHour = 0, int rMinute = 0, char *classify = defaultClassify, int priority = 1)
+{
+    this->id = id; //！！！！！！！！！！随机分配id应该写在这里！！！！！！！！！！！
+    int i = 0;
+    for (; name[i] != '\0'; ++i)
+    {
+        this->name[i] = name[i];
+    }
+    this->name[i] = '\0';
+    this->start = start;
+    i = 0;
+    for (; classify[i] != '\0'; ++i)
+    {
+        this->classify[i] = classify[i];
+    }
     this->classify[i] = '\0';
-    this->priority=priority;
-    this->timerReminder=timerReminder;
-    this->start.year=sYear;
-    this->start.month=sMonth;
-    this->start.day=sDay;
-    this->start.hour=sHour;
-    this->start.minute=sMinute;
-    this->timerReminder.year=rYear;
-    this->timerReminder.month=rMonth;
-    this->timerReminder.day=rDay;
-    this->timerReminder.hour=rHour;
-    this->timerReminder.minute=rMinute;
+    this->priority = priority;
+    this->timerReminder = timerReminder;
+    this->start.year = sYear;
+    this->start.month = sMonth;
+    this->start.day = sDay;
+    this->start.hour = sHour;
+    this->start.minute = sMinute;
+    this->timerReminder.year = rYear;
+    this->timerReminder.month = rMonth;
+    this->timerReminder.day = rDay;
+    this->timerReminder.hour = rHour;
+    this->timerReminder.minute = rMinute;
     taskNum++;
     taskLive++;
-    }
+}
 
-void delTask(task *myTask){
+void delTask(task *myTask)
+{
     int deleteId;
     cout << "请输入你要删除的任务对应的id: ";
     cin >> deleteId;
-    if(deleteId <=0 ){
+    if (deleteId <= 0)
+    {
         cout << "任务id不存在!" << endl;
     }
-    int i=0;
-    for(;i<LIST_SIZE;i++){
-        if(myTask[i].id==deleteId) break;
+    int i = 0;
+    for (; i < LIST_SIZE; i++)
+    {
+        if (myTask[i].id == deleteId)
+            break;
     }
-    if(i==LIST_SIZE){
-        cout<<"任务id不存在!"<<endl;
+    if (i == LIST_SIZE)
+    {
+        cout << "任务id不存在!" << endl;
         return;
     }
-    for(int j=i;j<LIST_SIZE-1;j++){
-        myTask[j]=myTask[j+1];
+    for (int j = i; j < LIST_SIZE - 1; j++)
+    {
+        myTask[j] = myTask[j + 1];
     }
     task::taskLive--;
-    cout<<"此任务已删除!"<<endl;
+    cout << "此任务已删除!" << endl;
     return;
-}//删除任务
+} //删除任务
 
-    void reminder(task t){
-	time_t timep;
-	struct tm *nowTime;
-	time(&timep); //获取从1970至今过了多少秒，存入time_t类型的timep
-	nowTime = localtime(&timep);//用localtime将秒数转化为struct tm结构体
-        if ((nowTime->tm_year+1900) == t.timerReminder.year && (1 + nowTime->tm_mon) == t.timerReminder.month && nowTime->tm_mday == t.timerReminder.day && nowTime->tm_hour == t.timerReminder.hour && nowTime->tm_min == t.timerReminder.minute){
-            cout << "现在" << t.timerReminder.hour <<"时" << t.timerReminder.minute << "分," << "您在" << t.start.hour <<"时" << t.start.minute << "分有任务：" << t.name << endl;
-            
+void reminder(task t)
+{
+    time_t timep;
+    struct tm *nowTime;
+    time(&timep);                //获取从1970至今过了多少秒，存入time_t类型的timep
+    nowTime = localtime(&timep); //用localtime将秒数转化为struct tm结构体
+    if ((nowTime->tm_year + 1900) == t.timerReminder.year && (1 + nowTime->tm_mon) == t.timerReminder.month && nowTime->tm_mday == t.timerReminder.day && nowTime->tm_hour == t.timerReminder.hour && nowTime->tm_min == t.timerReminder.minute)
+    {
+        cout << "现在" << t.timerReminder.hour << "时" << t.timerReminder.minute << "分,"
+             << "您在" << t.start.hour << "时" << t.start.minute << "分有任务：" << t.name << endl;
+    }
+    else
+        return;
+}; //提醒任务
+
+void showTaskByDay(task *myTask, int year, int month, int day)
+{
+    int loc[LIST_SIZE] = {-1}; //用来记录每个任务的位置
+    int num = 0;               //用来记录任务的个数
+    for (int i = 0; i < LIST_SIZE && myTask[i].id != 0; i++)
+    {
+        if (myTask[i].start.year == year && myTask[i].start.month == month && myTask[i].start.day == day)
+        {
+            loc[num] = i;
+            num++;
         }
-        else 
-            return;
-    };//提醒任务
+    } //查找day匹配的Task并记录其下标，存储于loc数组
 
-
-    void showTaskByDay(task *myTask,int year, int month, int day){
-        int loc[LIST_SIZE]={-1};//用来记录每个任务的位置
-        int num=0;//用来记录任务的个数
-        for(int i=0;i<LIST_SIZE && myTask[i].id!=0;i++){
-            if(myTask[i].start.year==year&&myTask[i].start.month==month&&myTask[i].start.day==day){
-                loc[num]=i;
-                num++;
+    bool flag; //记录起泡中是否发生交换
+    int i, j, temp;
+    for (i = 0; i < num; i++)
+    {
+        flag = false;
+        for (j = 0; j < num - i; j++)
+        {
+            if ((myTask[loc[j + 1]].start.hour < myTask[loc[j]].start.hour) || (myTask[loc[j + 1]].start.hour == myTask[loc[j]].start.hour && myTask[loc[j + 1]].start.minute < myTask[loc[j]].start.minute))
+            {
+                temp = loc[j];
+                loc[j] = loc[j + 1];
+                loc[j + 1] = temp;
+                flag = true;
             }
-        }//查找day匹配的Task并记录其下标，存储于loc数组
-
-        bool flag;//记录起泡中是否发生交换
-        int i,j,temp;
-        for( i=0;i<num;i++){
-            flag=false;
-            for( j=0;j<num-i;j++){
-                if((myTask[loc[j+1]].start.hour < myTask[loc[j]].start.hour) || (myTask[loc[j+1]].start.hour == myTask[loc[j]].start.hour && myTask[loc[j+1]].start.minute < myTask[loc[j]].start.minute)){
-                    temp=loc[j];
-                    loc[j]=loc[j+1];
-                    loc[j+1]=temp;
-                    flag=true;
-                }
-            }
-            if(!flag) break;
-        }//按照时间冒泡排序
-
-        for (i=0; i<num; i++){
-            myTask[loc[i]].display();
         }
-    }//显示某一天的任务
+        if (!flag)
+            break;
+    } //按照时间冒泡排序
 
+    for (i = 0; i < num; i++)
+    {
+        myTask[loc[i]].display();
+    }
+} //显示某一天的任务
 
-
-        void showTaskByMonth(task *myTask,int year, int month){
-        int loc[LIST_SIZE]={-1};//用来记录每个任务的位置
-        int num=0;//用来记录任务的个数
-        for(int i=0;i<LIST_SIZE && myTask[i].id!=0;i++){
-            if(myTask[i].start.year==year&&myTask[i].start.month==month){
-                loc[num]=i;
-                num++;
-            }
-        }//查找month匹配的Task并记录其下标，存储于loc数组
-
-        bool flag;//记录起泡中是否发生交换
-        int i,j,temp;
-        for( i=0;i<num;i++){
-            flag=false;
-            for( j=0;j<num-i;j++){
-                if((myTask[loc[j+1]].start.day<myTask[loc[j]].start.day) || (myTask[loc[j+1]].start.day=myTask[loc[j]].start.day&&myTask[loc[j+1]].start.hour < myTask[loc[j]].start.hour) || (myTask[loc[j+1]].start.day=myTask[loc[j]].start.day && myTask[loc[j+1]].start.hour == myTask[loc[j]].start.hour && myTask[loc[j+1]].start.minute < myTask[loc[j]].start.minute)){
-                    temp=loc[j];
-                    loc[j]=loc[j+1];
-                    loc[j+1]=temp;
-                    flag=true;
-                }
-            }
-            if(!flag) break;
-        }//按照时间冒泡排序
-
-        for (i=0; i<num; i++){
-            myTask[loc[i]].display();
+void showTaskByMonth(task *myTask, int year, int month)
+{
+    int loc[LIST_SIZE] = {-1}; //用来记录每个任务的位置
+    int num = 0;               //用来记录任务的个数
+    for (int i = 0; i < LIST_SIZE && myTask[i].id != 0; i++)
+    {
+        if (myTask[i].start.year == year && myTask[i].start.month == month)
+        {
+            loc[num] = i;
+            num++;
         }
-    }//显示某一月的任务
+    } //查找month匹配的Task并记录其下标，存储于loc数组
+
+    bool flag; //记录起泡中是否发生交换
+    int i, j, temp;
+    for (i = 0; i < num; i++)
+    {
+        flag = false;
+        for (j = 0; j < num - i; j++)
+        {
+            if ((myTask[loc[j + 1]].start.day < myTask[loc[j]].start.day) || (myTask[loc[j + 1]].start.day = myTask[loc[j]].start.day && myTask[loc[j + 1]].start.hour < myTask[loc[j]].start.hour) || (myTask[loc[j + 1]].start.day = myTask[loc[j]].start.day && myTask[loc[j + 1]].start.hour == myTask[loc[j]].start.hour && myTask[loc[j + 1]].start.minute < myTask[loc[j]].start.minute))
+            {
+                temp = loc[j];
+                loc[j] = loc[j + 1];
+                loc[j + 1] = temp;
+                flag = true;
+            }
+        }
+        if (!flag)
+            break;
+    } //按照时间冒泡排序
+
+    for (i = 0; i < num; i++)
+    {
+        myTask[loc[i]].display();
+    }
+} //显示某一月的任务
+using namespace std;
+void saveTask(char *password, task *mytask, char *userfile)
+{
+    FILE *fp;
+    //int len = strlen(userfile);
+    if ((fp = fopen(userfile, "a+")) == NULL)
+    {
+        fprintf(stderr, "Fail to open file!\n");
+        exit(0);
+    }
+    int i = 0;
+    fputs(encrypt(password), fp);
+    while ((mytask[i].id) != 0)
+    {
+        fputs(Int2String(mytask[i].id), fp);
+        fputs(mytask[i].name, fp);
+        fputs(Int2String(mytask[i].start.year), fp);
+        fputs(Int2String(mytask[i].start.month), fp);
+        fputs(Int2String(mytask[i].start.day), fp);
+        fputs(Int2String(mytask[i].start.hour), fp);
+        fputs(Int2String(mytask[i].start.minute), fp);
+        fputs(mytask[i].classify, fp);
+        fputs(Int2String(mytask[i].priority), fp);
+        fputs(Int2String(mytask[i].timerReminder.year), fp);
+        fputs(Int2String(mytask[i].timerReminder.month), fp);
+        fputs(Int2String(mytask[i].timerReminder.day), fp);
+        fputs(Int2String(mytask[i].timerReminder.hour), fp);
+        fputs(Int2String(mytask[i].timerReminder.minute), fp);
+        i++;
+    }
+    fclose(fp);
+}
+void loadTask(task mytask[], char *userfile)
+{
+    FILE *fp;
+    char mypassword[20], tmp[30]; // mypassword是用来储存第一行的密码的，tmp为临时储存的变量
+    //int len = strlen(userfile);
+    if ((fp = fopen(userfile, "r+")) == NULL)
+    {
+        fprintf(stderr, "Fail to open file!\n");
+        exit(0);
+    }
+    int tasknum = 0, tmpnumber = 0; // tasknum用来收集task数目，tmpnumber用来收集task的第i位值
+    fgets(mypassword, 20, fp);
+    while (fgets(tmp, 30, fp) != NULL)
+    {
+        switch (tmpnumber)
+        {
+        case 0:
+        {
+            if (tasknum > 0 && (String2Int(tmp) - mytask[tasknum - 1].id) > 1) //判断一下之前是否删除过这个区间的任务
+                tasknum = tasknum + (String2Int(tmp) - mytask[tasknum - 1].id) - 1;
+            mytask[tasknum].id = String2Int(tmp);
+            tmpnumber++;
+            break;
+        }
+        case 1:
+            strcpy(mytask[tasknum].name, tmp);
+            tmpnumber++;
+            break;
+        case 2:
+            mytask[tasknum].start.year = String2Int(tmp);
+            tmpnumber++;
+            break;
+        case 3:
+            mytask[tasknum].start.month = String2Int(tmp);
+            tmpnumber++;
+            break;
+        case 4:
+            mytask[tasknum].start.day = String2Int(tmp);
+            tmpnumber++;
+            break;
+        case 5:
+            mytask[tasknum].start.hour = String2Int(tmp);
+            tmpnumber++;
+            break;
+        case 6:
+            mytask[tasknum].start.minute = String2Int(tmp);
+            tmpnumber++;
+            break;
+        case 7:
+            strcpy(mytask[tasknum].classify, tmp);
+            tmpnumber++;
+            break;
+        case 8:
+            mytask[tasknum].priority = String2Int(tmp);
+            tmpnumber++;
+            break;
+        case 9:
+            mytask[tasknum].timerReminder.year = String2Int(tmp);
+            tmpnumber++;
+            break;
+        case 10:
+            mytask[tasknum].timerReminder.month = String2Int(tmp);
+            tmpnumber++;
+            break;
+        case 11:
+            mytask[tasknum].timerReminder.day = String2Int(tmp);
+            tmpnumber++;
+            break;
+        case 12:
+            mytask[tasknum].timerReminder.hour = String2Int(tmp);
+            tmpnumber++;
+            break;
+        case 13:
+            mytask[tasknum].timerReminder.minute = String2Int(tmp);
+            tmpnumber = 0;
+            tasknum++;
+            break;
+        }
+    }
+}
--- task.h
+++ task.h
@@ -14,7 +14,11 @@
     };
 
     class task{
+        friend void saveTask(char *password, task *mytask, char *userfile);
+        friend void loadTask(task mytask[], char *userfile);
         friend void delTask(task *myTask);
+        friend char *encrypt(char *password);
+        friend void checkpassword();
         friend void reminder(task t);
         friend void showTaskByDay(task *myTask,int year, int month, int day);
         friend void showTaskByMonth(task *myTask,int year, int month);
@@ -42,4 +46,8 @@ void reminder(task t);//提醒任务
 
 void showTaskByDay(task *myTask,int year, int month, int day);//按天显示任务
 void showTaskByMonth(task *myTask,int year, int month);//按月显示任务
+void checkpassword();//还没写
+char *encrypt(char *password);//还没写
+void loadTask(task *mytask,char *userfile);//写完了
+void saveTask(char *password,task *mytask,char *userfile);//写完了
 #endif
