1cdf4215b6c6fbd7b227f408b93701d34a766942
BowenYoung <yys958257943@gmail.com>
2022-07-08 14:53:09 +0800

    解决了显示任务时的bug

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ void usage(const char * cmd)
 {
 	printf("离线日程管理系统\n");
 	printf("可选参数   :  %s [-r][-a][-s][-d][-h]\n", cmd);
-    printf("-r : 运行程序\n-a : 添加任务\n-s xxxx-xx或xxxx-xx-xx\n-d <num> : 删除id为num的任务\n-h : 查看帮助\n");
+    printf("-r : 运行程序\n-a : 添加任务\n-s xxxx-xx或xxxx-xx-xx : 查看某月或某日的任务\n-d <num> : 删除id为num的任务\n-h : 查看帮助\n");
 
 	printf("示例:\n");
 	printf("   %s  -r\n",  cmd);
@@ -28,41 +28,50 @@ int main(int argc, char * argv[])
 {
     int opt;
     const char *optstring = "ras:d:h"; // 命令行参数有五个选项-rasdh，分别表示run add show del help
-    printf("请先登录……\n");//需要修改，如果没有用户则注册，如果有用户则登录
     while ((opt = getopt(argc, argv, optstring)) != -1) {
         switch (opt) {
             case 'r':
+                printf("请先登录……\n");//需要修改，如果没有用户则注册，如果有用户则登录
                 printf("程序正在运行……输入help查看帮助\n");
                 break;
             case 'a':
+                printf("请先登录……\n");//需要修改，如果没有用户则注册，如果有用户则登录
                 myTask[task::taskLive].setTask();
                 break;
             case 's':
-                printf("请先登录\n");
+                printf("请先登录……\n");//需要修改，如果没有用户则注册，如果有用户则登录
                 if (strlen(optarg)==10){
                     int year,month,day;
-                    char temp[10];
-                    strncpy(temp,optarg,4);
-                    year=atoi(temp);
-                    strncpy(temp,optarg+5,2);
-                    month=atoi(temp);
-                    strncpy(temp,optarg+8,2);
-                    day=atoi(temp);
+                    char temp1[5];
+                    char temp2[3];
+                    char temp3[3];
+                    strncpy(temp1,optarg,4);
+                    temp1[4]='\0';
+                    year=atoi(temp1);
+                    strncpy(temp2,optarg+5,2);
+                    temp2[2]='\0';
+                    month=atoi(temp2);
+                    strncpy(temp3,optarg+8,2);
+                    temp3[2]='\0';
+                    day=atoi(temp3);
                     showTaskByDay(myTask,year,month,day);
                 }
                 else if (strlen(optarg)==7){
                     int year,month;
-                    char temp[7];
-                    strncpy(temp,optarg,4);
-                    year=atoi(temp);
-                    strncpy(temp,optarg+5,2);
-                    month=atoi(temp);
+                    char temp1[5];
+                    char temp2[3];
+                    strncpy(temp1,optarg,4);
+                    temp1[4]='\0';
+                    year=atoi(temp1);
+                    strncpy(temp2,optarg+5,2);
+                    temp2[2]='\0';
+                    month=atoi(temp2);
                     showTaskByMonth(myTask,year,month);
                 }
                 else {printf("参数错误\n");usage(argv[0]);}
                 break;
             case 'd':
-                printf("请先登录\n");
+                printf("请先登录……\n");//需要修改，如果没有用户则注册，如果有用户则登录
                 delTask(myTask,atoi(optarg));
                 break;
             case 'h':
