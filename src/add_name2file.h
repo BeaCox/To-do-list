7e5bd197031f3e66f0c25ffe25ee6b05a34d36ad
skywalker107 <1315498689@qq.com>
2022-07-13 09:51:22 +0800

    src

---
+++ src/add_name2file.h
@@ -0,0 +1,4 @@
+#ifndef ADD_NAME2FILE_H
+#define ADD_NAME2FILE_H
+char *name2File(char *myname); //将用户名改成以.txt结尾的文件
+#endif
---
+++ src/deleteSpace.cpp
@@ -0,0 +1,7 @@
+#include "deleteSpace.h"
+#include <cstring>
+char *deleteSpace(char *str)
+{
+    str[strlen(str) - 1] = '\0';
+    return str;
+}
